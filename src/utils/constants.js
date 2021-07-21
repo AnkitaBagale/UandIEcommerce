d140db9507cbe4ba1d47d212db1e5f25cdc641b1
ankitabagale <manali.bagale@gmail.com>
2021-07-21 22:44:40 +0530

    fix: api url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-// export const API_URL = 'https://uandistoreapi.herokuapp.com';
-export const API_URL = 'http://localhost:3000';
+export const API_URL = 'https://uandistoreapi.herokuapp.com';
+// export const API_URL = 'http://localhost:3000';
 
 export const INDIAN_RUPEE = `\u20B9`;
